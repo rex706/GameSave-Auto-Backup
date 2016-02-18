d56796795b8b85f90621e455735b6f8191089bad
rex706 <apsinakis@me.com>
2016-02-17 20:58:37 -0500

    Bug fixes and new features
    - Fixed DropBox file path check.
    - Added internet connection check.
    - Will only fetch and push files that were changed and no longer prompts
    for overwriting files
    - Added Sims 4 icon

--- GameSave Auto-Backup/GameSave Auto-Backup.cpp
+++ GameSave Auto-Backup/GameSave Auto-Backup.cpp
@@ -8,9 +8,10 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <windows.h>
-#include <vector>
+#include <wininet.h>
+#pragma comment(lib,"Wininet.lib")
+#include <shlobj.h>
 #include <string>
-#include "time.h"
 #include <iostream>
 using namespace std;
 
@@ -19,11 +20,11 @@ void dirExists(const char *path){
 	struct stat info;
 
 	if (stat(path, &info) != 0)
-		printf("Cannot access %s\n\n", path);
+		printf("\nCannot access %s\n\n", path);
 	else if (info.st_mode & S_IFDIR)
-		printf("DropBox directory found!\n\n");
+		printf("\nDropBox directory found! '%s'\n", path);
 	else
-		printf("Unable to locate DropBox directory. Are you connected to the Internet?\n\n");
+		printf("\nUnable to locate DropBox directory. Make sure it is installed and signed in.\n");
 }
 
 //gets path where exe was opened from (should be the game folder)
@@ -34,24 +35,35 @@ string ExePath() {
 	return string(buffer).substr(0, pos);
 }
 
-int main()
-{
+int main(int argc, char** argv){
 
 	//check for internet connection. if no internet display error and ask if still want to launch game 
+	if (InternetCheckConnection(L"http://www.google.com", FLAG_ICC_FORCE_CONNECTION, 0))
+		cout << "\nConnected to the Internet!\n";
+
+	else {
+		cout << "\nWARNING: No Internet connection found. Save files cannot be synced. Play anyway? Y/N\n";
+	
+		char response;
+		cin >> response;
+
+		if (response == 'N' || response == 'n' || response == 'No' || response == 'no') {
+			return 0;
+		}
+	}
+
 	//check if dropbox directory exists
-	//should probably find a way to compare date modified to skip retrieving files if they are the same
-
-	std::string gamePath = ExePath() + "\\ts4.exe";
-
-	const char *path = "\%UserProfile%\\Dropbox\\saves\0";
+	char path[MAX_PATH];
+	SHGetFolderPathA(NULL, CSIDL_PROFILE, NULL, 0, path);
+	strcat_s(path, "\\Dropbox\\saves");
 	dirExists(path);
 
-	cout << "Loading save files from DropBox...\n";
-	system("xcopy \"\%UserProfile%\\Dropbox\\saves\" \"\%userProfile%\\Documents\\Electronic Arts\\Sims 4\\saves\" /e /i /j");
-
-	//find way to bypass overwrite prompt or auto input 'a' to indicate 'overwrite all'
+	//fetch files from DropBox
+	cout << "\nLoading save files from DropBox...\n";
+	system("xcopy \"\%UserProfile%\\Dropbox\\saves\" \"\%UserProfile%\\Documents\\Electronic Arts\\The Sims 4\\saves\" /e /i /j /d /y");
 	
-	//convert string to stupid LPWSTR type
+	//convert string to stupid LPWSTR type so it is compatable with CreateProcess function
+	std::string gamePath = ExePath() + "\\ts4.exe";
 	int bufferlen = ::MultiByteToWideChar(CP_ACP, 0, gamePath.c_str(), gamePath.size(), NULL, 0);
 	LPWSTR widestr = new WCHAR[bufferlen + 1];
 	::MultiByteToWideChar(CP_ACP, 0, gamePath.c_str(), gamePath.size(), &widestr[0], bufferlen);
@@ -62,22 +74,26 @@ int main()
 	PROCESS_INFORMATION processInfo;
 	if (CreateProcess(widestr, NULL, NULL, NULL, TRUE, 0, NULL, NULL, &info, &processInfo))
 	{
+		cout << "\nStarting Game!\n";
 		::WaitForSingleObject(processInfo.hProcess, INFINITE);
 		CloseHandle(processInfo.hProcess);
 		CloseHandle(processInfo.hThread);
+		cout << "\nGame closing...\n";
 	}
 	else {
-		cout << "ERROR could not find game executable!\n\n";
+		cout << "ERROR: Could not find game executable! Is the launcher in the correct directory?\n\n";
 		system("PAUSE");
 		return 0;
 	}
 
 	delete[] widestr;
 
-	//need to figure out how to only upload files that changed instead of entire directory
-	cout << "\nUploading save files to DropBox...\n";
-	system("xcopy \"\%UserProfile%\\Documents\\Electronic Arts\\Sims 4\\saves\" \"\%userProfile%\\Dropbox\\saves\" /e /i /j");
+	//find a way to deal with multiple people playing and saving at the same time
 
+	//push files to DropBox
+	cout << "\nUploading save files to DropBox...\n";
+	system("xcopy \"\%UserProfile%\\Documents\\Electronic Arts\\The Sims 4\\saves\" \"\%UserProfile%\\Dropbox\\saves\" /e /i /j /d /y");
+	cout << "\nYour saves have been copied! It is now safe to exit.";
 	system("PAUSE"); //debug
     return 0;
 }
